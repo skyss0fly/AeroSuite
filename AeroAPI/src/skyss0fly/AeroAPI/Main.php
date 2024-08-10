54fd24a7ddf1492fc90fffc65f9dfb86c184e3d4
skyss0fly-pm-pl <144737802+skyss0fly-pm-pl@users.noreply.github.com>
2024-08-10 13:21:09 +1000

    Update Main.php

--- AeroAPI/src/skyss0fly/AeroAPI/Main.php
+++ AeroAPI/src/skyss0fly/AeroAPI/Main.php
@@ -1,28 +1,42 @@
+
 <?php
 
-namespace skyss0fly/AeroAPI;
+namespace skyss0fly\AeroAPI;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\player\Player;
 
 class Main extends PluginBase {
-private $money;
-public function onLoad(): void {
-$this->money = new([Player, Amount]);
+    private $money = [];
 
+    public function onLoad(): void {
+        // Initialize the money array
+        $this->money = [];
+    }
 
-  
+    public function addMoney(Player $player, int $amount): bool {
+        $playerName = $player->getName();
+        
+        // Check if the player already has money recorded
+        if (isset($this->money[$playerName])) {
+            $previous = $this->money[$playerName];
+        } else {
+            $previous = 0;
+        }
+
+        // Calculate the new amount
+        $new = $previous + $amount;
+        $this->money[$playerName] = $new;
+
+        // Return true to indicate the operation was successful
+        return true;
+    }
+
+    public function getMoney(Player $player): int {
+        $playerName = $player->getName();
+
+        // Return the player's money or 0 if not set
+        return $this->money[$playerName] ?? 0;
+    }
 }
 
-  public function addMoney($player Player, $amount args[1]) bool {
-    $previous = args[1]->this->money($player, $amount);
-    $new = $ previous + $amount[1];
-    
-return $this->money($player, $new);    
-  }
-
-}
-
-
-  }
-}
