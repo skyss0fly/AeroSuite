879b3ff8e8a0b59ab3fbd743a4ba76deb1826a0d
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2024-10-17 02:17:01 +1000

    Update AeroLCCommand.php

--- AeroLC/src/skyss0fly/AeroLC/command/AeroLCCommand.php
+++ AeroLC/src/skyss0fly/AeroLC/command/AeroLCCommand.php
@@ -1,11 +1,27 @@
 <?php
 namespace skyss0fly\AeroLC;
+
 use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
 
 class AeroLCCommand extends Command {
-/*
-todo: make base command
-*/
 
+    public function __construct(){
+        parent::__construct("aerolc", "AeroLC base command", "/aerolc", []);
+        $this->setPermission("aerolc.command");
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+        if(!$this->testPermission($sender)){
+            return false;
+        }
+
+        if($sender instanceof Player){
+            $sender->sendMessage("This is the base AeroLC command!");
+        } else {
+            $sender->sendMessage("This command can only be used in-game.");
+        }
+        return true;
+    }
 }
-<?
