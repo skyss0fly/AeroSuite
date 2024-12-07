56046f0a27805f41dc994709f96f1c00bdfc8853
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2024-12-07 21:21:59 +1000

    Update Position1.php

--- AeroLC/src/skyss0fly/AeroLC/command/subcommand/Position1.php
+++ AeroLC/src/skyss0fly/AeroLC/command/subcommand/Position1.php
@@ -10,15 +10,14 @@ use CortexPE\Commando\BaseSubCommand;
 
 use skyss0fly\AeroLC\AeroLC;
 use skyss0fly\AeroLC\utils\ConfigManager;
-use skyss0fly\AeroLC\utils\LangManager;
 
-class HelpSubcommand extends BaseSubCommand {
+class Position1Subcommand extends BaseSubCommand {
     protected function prepare(): void {
         $this->setPermission("AeroLC.command.pos1");
     }
     
     public function onRun(CommandSender $sender, string $alias, array $args): void {
-        $lang_manager = LangManager::getInstance();
+        
         $this->setFirstPosition($sender->getPosition()->getX(), $sender->getPosition()->getY(), $sender->getPosition()->getZ());
         //TODO
     }
