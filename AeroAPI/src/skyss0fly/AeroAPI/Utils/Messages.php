74c258b923b4d7550ad7171e7f7692199ff694ef
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2025-03-11 17:50:58 +1000

    Update Messages.php

--- AeroAPI/src/skyss0fly/AeroAPI/Utils/Messages.php
+++ AeroAPI/src/skyss0fly/AeroAPI/Utils/Messages.php
@@ -1,19 +1,22 @@
 <?php
+
 namespace skyss0fly\AeroAPI;
+
 use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 
-class Messages {
+class Messages extends PluginBase {
 
-public function sendErrorToConsole(string $str){
-  $this->getLogger()->error("AeroAPI: " . $str);
-}
+    public function sendErrorToConsole(string $str): void {
+        $this->getLogger()->error("AeroAPI: " . $str);
+    }
 
-public function sendErrorToPlayers(string $str, player $player){
-if (!$player instanceof Player){
-$this->sendErrorToConsole("Player Not Found!");
+    public function sendErrorToPlayers(string $str, Player $player): void {
+        if (!$player instanceof Player) {
+            $this->sendErrorToConsole("Player Not Found!");
+            return;
+        }
+        $player->sendMessage(TextFormat::RED . "AeroAPI: " . $str);
+    }
 }
-else {
-$player->sendMessage(TEXTFORMAT::RED . "AeroAPI: " . $str);
-}
-?>
